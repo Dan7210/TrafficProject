9ed53d1a27627f43df9faab254eb6ca25cda9477
DESKTOP-9JO4DE2\Daniel Marquez <merkytv22@gmail.com>
2022-04-19 10:37:19 -0500

    New files to control pi zero

---
+++ LedRun.java
@@ -0,0 +1,30 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class LedRun {
+    private static GpioController gpio = null;
+    private static GpioPinDigitalOutput red = null;
+    public static void main(String[] args) throws InterruptedException {
+        //When program ends, turn off LEDs and shutdown gpio controller
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                red.low();
+                gpio.shutdown();
+            }
+        });
+
+        gpio = GpioFactory.getInstance();
+        red = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "Red", PinState.LOW);
+
+        while(true) {
+            red.high();
+            Thread.sleep(3000);
+            red.low();
+            Thread.sleep(3000);
+        }        
+    }
+}
