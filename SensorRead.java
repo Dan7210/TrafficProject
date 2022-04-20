9085fbe0c212c8cec1f3b854abe53dd8744b5dda
DESKTOP-9JO4DE2\Daniel Marquez <merkytv22@gmail.com>
2022-04-20 15:33:54 -0500

    Changes to program

--- SensorRead.java
+++ SensorRead.java
@@ -1,42 +1,47 @@
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
-public class SensorRead {
-    private static GpioController gpio = null;
-    private static GpioPinDigitalInput sensor = null;
-    
-    public static void main(String[] args) throws InterruptedException {
-        gpio = GpioFactory.getInstance();
-        GpioPinDigitalInput sensor = gpio.provisionDigitalInputPin(RaspiPin.GPIO_12, "sensor");
-        System.out.println("Listener activated.");
-        while(true) {
-            checkLED();
-        }
-    }
+/**
+ * This example code demonstrates how to setup a listener
+ * for GPIO pin state changes on the Raspberry Pi.
+ *
+ * @author Robert Savage
+ */
 
-    public static void checkLED() throws InterruptedException {
-        try {
-            if(sensor != null){
-                sensor.addListener(new GpioPinListenerDigital() {
-                @Override
-                public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                    System.out.println("GPIO Pin: " + event.getPin() + " = " + event.getState());
-                }
-                });
+public class SensorRead {
+
+    public static void main(String args[]) throws InterruptedException {
+        System.out.println("<--Pi4J--> GPIO Listen Example ... started.");
+
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+
+        // set shutdown state for this input pin
+        myButton.setShutdownOptions(true);
+
+        // create and register gpio pin listener
+        myButton.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
             }
-            else {
-                System.out.println("Listener is null.");
-            }
-    
-            /*while(true) {
-                Thread.sleep(500);
-            }*/
-        } catch(Exception e) {
-            e.printStackTrace();
+
+        });
+
+        System.out.println(" ... complete the GPIO #02 circuit and see the listener feedback here in the console.");
+
+        // keep program running until user aborts (CTRL-C)
+        while(true) {
+            Thread.sleep(500);
         }
+
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+        // gpio.shutdown();   <--- implement this method call if you wish to terminate the Pi4J GPIO controller
     }
 }
